637ccb89aef81b22745ce7caac888ac0bd07b1c1
sergkovtunenko-lgtm <sergkovtunenko@gmail.com>
2026-04-12 22:23:01 +0300

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help='Choose language: en, es, fr, etc.')
+
+@pytest.fixture(scope="session")
+def browser(request):
+    language = request.config.getoption("--language")
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    driver.quit()
